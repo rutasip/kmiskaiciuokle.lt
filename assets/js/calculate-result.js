2e069809cc58e3985e0281c1b2743ff38a800d9f
rutasip <ruta.sipkauskaite@gmail.com>
2021-11-01 01:31:52 +0200

    handle required fields

--- assets/js/calculate-result.js
+++ assets/js/calculate-result.js
@@ -1,29 +1,45 @@
 function calculateBmi() {
-  const weight = document.getElementById("weight").value;
-  const height = document.getElementById("height").value;
-  const select = document.getElementById("gender");
-  const gender = select.options[select.selectedIndex].value;
+  const form = document.getElementsByTagName("form")[0];
 
-  const bmi = (weight / ((height * height) / 10000)).toFixed(1);
+  if (form.checkValidity()) {
+    document
+      .getElementById("required-fields-error")
+      .classList.add("visually-hidden");
 
-  document.getElementById("calculated-bmi").innerText = bmi;
+    const weight = document.getElementById("weight").value;
+    const height = document.getElementById("height").value;
+    const select = document.getElementById("gender");
+    const gender = select.options[select.selectedIndex].value;
 
-  document.getElementById(this.highlightRow(bmi)).classList.add("bg-yellow");
+    const bmi = (weight / ((height * height) / 10000)).toFixed(1);
 
-  if (gender == "female") {
-    document.getElementById("ideal-weight").classList.remove("visually-hidden");
-    document.getElementById("calculated-ideal-weight").innerText = (
-      45.5 +
-      0.91 * (height - 152.4)
-    ).toFixed();
-  } else if (gender == "male") {
-    document.getElementById("ideal-weight").classList.remove("visually-hidden");
-    document.getElementById("calculated-ideal-weight").innerText = (
-      50 +
-      0.91 * (height - 152.4)
-    ).toFixed();
+    document.getElementById("calculated-bmi").innerText = bmi;
+
+    document.getElementById(this.highlightRow(bmi)).classList.add("bg-yellow");
+
+    if (gender == "female") {
+      document
+        .getElementById("ideal-weight")
+        .classList.remove("visually-hidden");
+      document.getElementById("calculated-ideal-weight").innerText = (
+        45.5 +
+        0.91 * (height - 152.4)
+      ).toFixed();
+    } else if (gender == "male") {
+      document
+        .getElementById("ideal-weight")
+        .classList.remove("visually-hidden");
+      document.getElementById("calculated-ideal-weight").innerText = (
+        50 +
+        0.91 * (height - 152.4)
+      ).toFixed();
+    } else {
+      document.getElementById("ideal-weight").classList.add("visually-hidden");
+    }
   } else {
-    document.getElementById("ideal-weight").classList.add("visually-hidden");
+    document
+      .getElementById("required-fields-error")
+      .classList.remove("visually-hidden");
   }
 }
 
