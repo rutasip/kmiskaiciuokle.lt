5d77d271fb95f99f8f7db84763cf3a35be4a9194
Rūta Šipkauskaitė <rutasip@Rutas-Air.lan>
2024-10-27 21:03:46 +0200

    upate sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,36 +1,37 @@
 import { Fragment, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { Disclosure } from "@headlessui/react";
 import { Dialog, Transition } from "@headlessui/react";
 import {
   Bars3Icon,
-  CalculatorIcon,
+  // CalculatorIcon,
   XMarkIcon,
-  //   ScaleIcon,
-  FireIcon,
-  // Battery100Icon,
-  BoltIcon,
-  // MoonIcon,
-  // ClockIcon,
-  // PuzzlePieceIcon,
-  ArrowPathIcon,
+  HeartIcon,
+  ScaleIcon,
+  // ArrowPathIcon,
 } from "@heroicons/react/24/outline";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
-const healthCalculatorsNavigation = [
-  { name: "Koks mano KMI?", href: "/", icon: CalculatorIcon },
-  { name: 'Kiek kalorijų sudeginu?', href: '/sudeginamos-kalorijos', icon: BoltIcon },
-  // { name: 'Kiek miego?', href: '/miego-ciklai', icon: BoltIcon },
-];
-
-const nutritionCalculatorsNavigation = [
-  { name: 'Kiek kalorijų man reikia?', href: '/kaloriju-poreikiai', icon: FireIcon },
-  { name: 'Kiek vandens man reikia?', href: '/vandens-norma', icon: ArrowPathIcon },
-  // { name: 'Kiek miego?', href: '/miego-ciklai', icon: BoltIcon },
-  // { name: 'Kiek kalorijų man reikia?', href: '/makroelementai', icon: PuzzlePieceIcon },
+const navigation = [
+  {
+    name: "Sveikatos skaičiuoklės",
+    icon: HeartIcon,
+    children: [
+      { name: "Koks mano KMI?", href: "/" },
+      { name: "Kiek kalorijų sudeginu?", href: "/sudeginamos-kalorijos" },
+    ],
+  },
+  {
+    name: "Mitybos skaičiuoklės",
+    icon: ScaleIcon,
+    children: [
+      { name: "Kiek kalorijų man reikia?", href: "/kaloriju-poreikiai" },
+      { name: "Kiek vandens man reikia?", href: "/vandens-norma" },
+    ],
+  },
 ];
 
 const otherNavigation = [
-  // { name: "Kontaktai", href: "/atsakomybes-ribojimas" },
-  // { name: "Privatumo ir slapukų politika", href: "/atsakomybes-ribojimas" },
   { name: "Atsakomybės ribojimas", href: "/atsakomybes-ribojimas" },
 ];
 
@@ -43,95 +44,67 @@ function NavItems() {
   const currentPath = location.pathname;
 
   return (
-    <div className="flex grow flex-col overflow-y-auto bg-navy-blue px-6">
-      <nav className="flex flex-1 flex-col">
-        <div>
-          <div className="mt-12 text-xs font-semibold leading-6 text-white">Sveikatos skaičiuoklės:</div>
-          <ul role="list" className="flex flex-1 flex-col gap-y-7">
-            <li>
-              <ul role="list" className="-mx-3 mt-2 space-y-1">
-                {healthCalculatorsNavigation.map((item) => (
-                  <li key={item.name}>
-                    <a
-                      href={item.href}
-                      className={classNames(
-                        item.href === currentPath
-                          ? "bg-gray-50 text-navy-blue"
-                          : "text-white hover:bg-gray-50 hover:text-navy-blue",
-                        "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
-                      )}
-                    >
-                      <item.icon
+    <div className="flex flex-col h-full bg-navy-blue pt-10">
+      <nav className="flex-1 flex flex-col justify-between px-3 py-4 overflow-y-auto">
+        <div className="flex flex-col gap-7">
+          {navigation.map((item) => (
+            <div key={item.name}>
+              <Disclosure as="div" defaultOpen>
+                {({ open }) => (
+                  <>
+                    <Disclosure.Button className="w-full flex items-center justify-between px-2 py-2 text-left text-sm font-medium text-white rounded-md">
+                      <div className="flex items-center">
+                        <item.icon
+                          className="mr-3 flex-shrink-0 h-5 w-5 text-white group-hover:text-navy-blue"
+                          aria-hidden="true"
+                        />
+                        {item.name}
+                      </div>
+                      <ChevronDownIcon
                         className={classNames(
-                          item.href === currentPath
-                            ? "text-navy-blue"
-                            : "text-white group-hover:text-navy-blue",
-                          "h-6 w-6 shrink-0"
+                          open ? "transform rotate-180" : "",
+                          "h-5 w-5 text-white group-hover:text-navy-blue"
                         )}
-                        aria-hidden="true"
                       />
-                      {item.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </li>
-          </ul>
-          <div className="mt-12 text-xs font-semibold leading-6 text-white">Mitybos skaičiuoklės:</div>
-          <ul role="list" className="flex flex-1 flex-col gap-y-7">
-            <li>
-              <ul role="list" className="-mx-3 mt-2 space-y-1">
-                {nutritionCalculatorsNavigation.map((item) => (
-                  <li key={item.name}>
-                    <a
-                      href={item.href}
-                      className={classNames(
-                        item.href === currentPath
-                          ? "bg-gray-50 text-navy-blue"
-                          : "text-white hover:bg-gray-50 hover:text-navy-blue",
-                        "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
-                      )}
-                    >
-                      <item.icon
-                        className={classNames(
-                          item.href === currentPath
-                            ? "text-navy-blue"
-                            : "text-white group-hover:text-navy-blue",
-                          "h-6 w-6 shrink-0"
-                        )}
-                        aria-hidden="true"
-                      />
-                      {item.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </li>
-          </ul>
+                    </Disclosure.Button>
+                    <Disclosure.Panel className="space-y-1">
+                      {item.children.map((subItem) => (
+                        <a
+                          key={subItem.name}
+                          href={subItem.href}
+                          className={classNames(
+                            subItem.href === currentPath
+                              ? "bg-white text-navy-blue"
+                              : "text-white hover:bg-white hover:text-navy-blue",
+                            "group flex items-center pl-2 pr-2 py-2 text-sm font-medium rounded-md"
+                          )}
+                        >
+                          {subItem.name}
+                        </a>
+                      ))}
+                    </Disclosure.Panel>
+                  </>
+                )}
+              </Disclosure>
+            </div>
+          ))}
         </div>
-      </nav>
-      <nav className="mb-3">
-        <ul role="list" className="flex flex-1 flex-col gap-y-7">
-          <li>
-            <ul role="list" className="-mx-3 mt-2 space-y-1">
-              {otherNavigation.map((item) => (
-                <li key={item.name}>
-                  <a
-                    href={item.href}
-                    className={classNames(
-                      item.href === currentPath
-                        ? "bg-gray-50 text-navy-blue"
-                        : "text-white hover:bg-gray-50 hover:text-navy-blue",
-                      "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
-                    )}
-                  >
-                    <span>{item.name}</span>
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </li>
-        </ul>
+
+        {otherNavigation.map((item) => (
+          <div key={item.name}>
+            <a
+              href={item.href}
+              className={classNames(
+                item.href === currentPath
+                  ? "bg-white text-navy-blue"
+                  : "text-white hover:bg-white hover:text-navy-blue",
+                "group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+              )}
+            >
+              {item.name}
+            </a>
+          </div>
+        ))}
       </nav>
     </div>
   );
@@ -142,77 +115,67 @@ export function Sidebar() {
 
   return (
     <>
+      {/* Mobile sidebar */}
       <Transition.Root show={sidebarOpen} as={Fragment}>
         <Dialog
           as="div"
-          className="relative z-50 lg:hidden"
+          className="relative z-40 lg:hidden"
           onClose={setSidebarOpen}
         >
           <Transition.Child
             as={Fragment}
-            enter="transition-opacity ease-linear duration-300"
+            enter="transition-opacity ease-linear duration-200"
             enterFrom="opacity-0"
             enterTo="opacity-100"
-            leave="transition-opacity ease-linear duration-300"
+            leave="transition-opacity ease-linear duration-200"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-gray-900/80" />
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
 
           <div className="fixed inset-0 flex">
             <Transition.Child
               as={Fragment}
-              enter="transition ease-in-out duration-300 transform"
+              enter="transition ease-in-out duration-200 transform"
               enterFrom="-translate-x-full"
               enterTo="translate-x-0"
-              leave="transition ease-in-out duration-300 transform"
+              leave="transition ease-in-out duration-200 transform"
               leaveFrom="translate-x-0"
               leaveTo="-translate-x-full"
             >
-              <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-in-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in-out duration-300"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
-                    <button
-                      type="button"
-                      className="-m-2.5 p-2.5"
-                      onClick={() => setSidebarOpen(false)}
-                    >
-                      <span className="sr-only">Close sidebar</span>
-                      <XMarkIcon
-                        className="h-6 w-6 text-white"
-                        aria-hidden="true"
-                      />
-                    </button>
-                  </div>
-                </Transition.Child>
+              <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-navy-blue pb-4">
+                <div className="flex items-center h-16 px-4">
+                  <button
+                    type="button"
+                    className="text-white"
+                    onClick={() => setSidebarOpen(false)}
+                  >
+                    <span className="sr-only">Uždaryti meniu</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+                </div>
                 <NavItems />
               </Dialog.Panel>
             </Transition.Child>
+            <div className="w-14 flex-shrink-0" aria-hidden="true" />
           </div>
         </Dialog>
       </Transition.Root>
 
-      {/* Static sidebar for desktop */}
-      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-74 lg:flex-col">
+      {/* Desktop sidebar */}
+      <div className="hidden lg:fixed lg:inset-y-0 lg:flex lg:w-74 lg:flex-col">
         <NavItems />
       </div>
 
-      <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+      {/* Mobile top bar */}
+      <div className="sticky top-0 z-10 flex items-center bg-white shadow px-4 py-2 lg:hidden">
         <button
           type="button"
-          className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+          className="text-gray-500"
           onClick={() => setSidebarOpen(true)}
         >
-          <span className="sr-only">Open sidebar</span>
+          <span className="sr-only">Atidaryti meniu</span>
           <Bars3Icon className="h-6 w-6" aria-hidden="true" />
         </button>
       </div>
